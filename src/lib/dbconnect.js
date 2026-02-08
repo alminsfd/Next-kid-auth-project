65055bbdb60a12984b9e91332802a50bf10aa0f3
alminsfd <alaminhossaintanvir42@gmail.com>
2026-02-08 12:37:42 +0600

    add google font setup

---
+++ src/lib/dbconnect.js
@@ -0,0 +1,17 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.URI
+const DB_name = process.env.DB_NAME
+const Collectins = {
+     Products: 'products',
+}
+const client = new MongoClient(uri, {
+     serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+     }
+});
+
+export const dbConnected = (cmane) => {
+     return client.db(DB_name).collection(cmane)
+}
